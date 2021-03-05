9a8773e5a38846ac6c91594c48d2ebd8f9f1beca
ezio <ezioreturner@gmail.com>
2021-03-05 17:14:00 +0800

    update

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => null;
+
+ReactDOM.render(<App />, document.getElementById('root'));
