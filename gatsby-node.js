9c04f690d47c8f612f34e313389a32c7fe68ad69
ezio <ezioreturner@gmail.com>
2021-03-08 11:24:07 +0800

    更新

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,7 @@ exports.onCreateWebpackConfig = args => {
       modules: [path.resolve(__dirname, '../src'), 'node_modules'],
       alias: {
         'mc-ui/lib': path.resolve(__dirname, '../src/components/'),
+        'mc-ui': path.resolve(__dirname, '../src/'),
       },
     },
   })
--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => null;
-
-ReactDOM.render(<App />, document.getElementById('root'));
