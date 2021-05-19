8310d107e77426bcce5ed907542c96f565c2e0a5
ezioreturner <ezioreturner@gmail.com>
2021-05-19 10:02:05 +0800

    feat: update models & theme

--- components/_models/global.d.ts
+++ components/_models/global.d.ts
@@ -7,3 +7,5 @@ declare type StoreValue = any;
 declare module 'react-feather/dist/icons/chevron-right';
 
 declare module 'react-feather/dist/icons/chevron-left';
+
+declare module 'react-feather/dist/icons/chevrons-left';
