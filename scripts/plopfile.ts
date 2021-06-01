9a4e2bab4ef491d925be64e7821394716d014968
ezioreturner <ezioreturner@gmail.com>
2021-06-01 15:43:24 +0800

    feat(plop): 调整plop代码，调整文档

--- scripts/plopfile.ts
+++ scripts/plopfile.ts
@@ -2,15 +2,22 @@
 import { NodePlopAPI } from 'plop';
 import path from 'path';
 
+const validateFn = (value: any) => {
+  if (!value) {
+    return '必填项';
+  } 
+  return true;
+}
+
 export default function(plop: NodePlopAPI) {
   plop.setGenerator('component', {
     description: '创建一个新组件',
     prompts: [
-      { type: 'input', name: 'name', message: '请输入组件名称（多个单词使用驼峰命名）' },
-      { type: 'input', name: 'CN', message: '请输入组件中文名称' },
+      { type: 'input', name: 'name', message: '请输入组件名称（多个单词使用短横线隔开式命名）', validate: validateFn},
+      { type: 'input', name: 'CN', message: '请输入组件中文名称', validate: validateFn },
       { type: 'input', name: 'description', message: '请输入组件描述' },
       { type: 'input', name: 'contributors', message: '请输入贡献者名称' },
-      { type: 'input', name: 'compType', message: '请输入组件所属菜单类别：功能[components]、布局[layout]' }
+      { type: 'list', name: 'compType', message: '请选择组件所属菜单类别：功能[components]、布局[layout]', choices: ['components', 'layout'] }
     ],
     actions: [
       {
@@ -52,13 +59,7 @@ export default function(plop: NodePlopAPI) {
         type: 'add',
         path: path.resolve(__dirname, '../components/{{kebabCase name}}/__tests__/index.test.tsx'),
         templateFile: path.resolve(__dirname, '../templates/component/__tests__/index.test.hbs'),
-      },
-      // {
-      //   type: 'append',
-      //   path: path.resolve(__dirname, '../components/index.ts'),
-      //   pattern: '/* PLOP_INJECT_EXPORT */',
-      //   template: "export { default as {{pascalCase name}} } from './{{kebabCase name}}';",
-      // },
+      }
     ],
   });
 }
