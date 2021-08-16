5aeedd12b3dfd34c84a930a82fb05c0cba904cbd
ezioreturner <ezioreturner@gmail.com>
2021-08-16 14:29:55 +0800

    fix: 入口补充

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,5 @@
 export { default as Pagination } from './pagination';
 export { default as Empty } from './empty';
+export { default as Drawer } from './drawer';
+export { default as Layout } from './layout';
+export { default as Table } from './table';
